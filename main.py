17d2c081226b9b91df5b59f9d938618af6c949f8
kennmundell <64756644+kennmundell@users.noreply.github.com>
2021-06-10 20:21:47 +0100

    Add files via upload
    main py now uses list comprehension to create list of states not guessed

--- main.py
+++ main.py
@@ -19,10 +19,13 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50", prompt="What's another state's name?").title()
 
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        #line below uses list comprehension to replace following 4 lines of code
+        missing_states = [state for state in all_states if state not in guessed_states]
+
+        #missing_states = []
+        #for state in all_states:
+            #if state not in guessed_states:
+               # missing_states.append(state)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("States to learn.csv")
 
